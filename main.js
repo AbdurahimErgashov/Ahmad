d397fab4609e1af48a45c8423ddf417d8b49e6f3
Abdurahim_Dev <ergashovabdurahim32@gmail.com>
2024-01-31 10:55:21 +0500

    akjdfhadsk

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-const A = document.querySelector(".input1").value;
-const B = document.querySelector(".input2").value;
-const C = document.querySelector(".input3").value;
+document.querySelector(".btn").addEventListener("click", Triangle);
 
-// function Triangle(e) {
-    if(/^[0-9]{0-5}$/.test(A)&&/^[0-9]{0-5}$/.test(B)&&/^[0-9]{0-5}$/.test(C)){
-        alert("ishladi!")
+function Triangle(event) {
+    const A = document.querySelector(".input1").value*1;
+    const B = document.querySelector(".input2").value*1;
+    const C = document.querySelector(".input3").value*1;
+    event.preventDefault();
+
+    if (/^[1-9][0-9]{0,4}$/.test(A) && /^[1-9][0-9]{0,4}$/.test(B) && /^[1-9][0-9]{0,4}$/.test(C)) {
+        let Arr = [A,B,C];
+        Arr.sort((a, b) => a - b);
+        let hNum = Arr[2];
+        let nNum = Arr[1];
+        let eNum = Arr[0];
+        if ((eNum+nNum>hNum&&nNum-eNum<hNum)||(eNum+hNum>nNum&&hNum-eNum<nNum)||(nNum+hNum>eNum&&hNum-nNum<eNum)) {
+            let p = (eNum+nNum+hNum)/2;
+            let S = (Math.sqrt(p*(p-eNum)*(p-nNum)*(p-hNum)));
+            document.querySelector(".peremetr").textContent = p + "sm";
+            document.querySelector(".yuza").textContent = S.toFixed(1) + "sm^2";
+        } else{
+            document.querySelector(".input1").value = '';
+            document.querySelector(".input2").value = '';
+            document.querySelector(".input3").value = '';
+            alert("Bu uzunliklar uchburchak yasash qoidalariga to'g'ri kelmaydi!!!🤬")
+        }
+    } else {
+        alert("Har bir maydon 1 xona dan 5 xona gacha faqat son bo'lishi kerak!");
     }
-    // console.log(e.target);
-// }
-console.log(A,B,C)
+}
