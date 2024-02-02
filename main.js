ec811092a6519ca8583c6c134a56a5cbcf8d2c59
Abdurahim_Dev <ergashovabdurahim32@gmail.com>
2024-02-02 13:31:47 +0500

    togirlandi

--- main.js
+++ main.js
@@ -7,16 +7,11 @@ function Triangle(event) {
     event.preventDefault();
 
     if (/^[1-9][0-9]{0,4}$/.test(A) && /^[1-9][0-9]{0,4}$/.test(B) && /^[1-9][0-9]{0,4}$/.test(C)) {
-        let Arr = [A,B,C];
-        Arr.sort((a, b) => a - b);
-        let hNum = Arr[2];
-        let nNum = Arr[1];
-        let eNum = Arr[0];
-        if ((eNum+nNum>hNum&&nNum-eNum<hNum)||(eNum+hNum>nNum&&hNum-eNum<nNum)||(nNum+hNum>eNum&&hNum-nNum<eNum)) {
-            let p = (eNum+nNum+hNum);
-            let S = (Math.sqrt(p*(p-eNum)*(p-nNum)*(p-hNum)));
-            document.querySelector(".peremetr").textContent = p + "sm";
-            document.querySelector(".yuza").textContent = S.toFixed(1) + "sm^2";
+        if ((A+B>C&&Math.abs(B-A)<C)||(A+C>B&&Math.abs(C-A)<B)||(B+C>A&&Math.abs(C-B)<A)) {
+            let p = (A+B+C);
+            let S = (A*B*C);
+            document.querySelector(".p1").textContent = p + " sm";
+            document.querySelector(".s1").textContent = S.toFixed(0) + " sm^2";
         } else{
             document.querySelector(".input1").value = '';
             document.querySelector(".input2").value = '';
@@ -27,3 +22,12 @@ function Triangle(event) {
         alert("Har bir maydon 1 xona dan 5 xona gacha faqat son bo'lishi kerak!");
     }
 }
+
+setInterval(() => {
+    const A = document.querySelector(".input1").value*1;
+    const B = document.querySelector(".input2").value*1;
+    const C = document.querySelector(".input3").value*1;
+    document.querySelector(".a_tomon").innerHTML = A
+    document.querySelector(".b_tomon").innerHTML = B
+    document.querySelector(".c_tomon").innerHTML = C
+}, 1000);
